dc6969c300d8c2f741f7d2c2c291036c8f144f22
ThunderSphun <manderijn2000@gmail.com>
2024-05-12 09:41:22 +0200

    added glfw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	if (!glfwInit()) {
+		return -1;
+	}
+	GLFWwindow* window = glfwCreateWindow(480, 360, "test", nullptr, nullptr);
+	if (!window){
+		glfwTerminate();
+		return -1;
+	}
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
